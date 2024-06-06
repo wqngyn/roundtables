00ade952c2c3c7665e03cb76d626d01cafbfc1d1
William Nguyen <101022886+wqngyn@users.noreply.github.com>
2024-06-05 20:05:58 -0700

    remove server folder

--- server/middleware/controllers/authController.js
+++
@@ -1,5 +0,0 @@
-const asyncHandler = require('express-async-handler');
-
-exports.authorization_get = asyncHandler(async(req, res, next) => {
-
-})
--- server/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const querystring = require("querystring"); 
-
-const auth_controller = require('../middleware/controllers/authController');
-
-router.get('/authorization', auth_controller.authorization_get);
-
-module.exports = router;
