00512378360a16690d642bdda31344a6a105dc5d
William Nguyen <101022886+wqngyn@users.noreply.github.com>
2024-06-05 21:26:20 -0700

    ts_1

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import Router from './Router.jsx'
 import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <Router />
-  // </React.StrictMode>,
+  </React.StrictMode>,
 )
