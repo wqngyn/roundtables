5616db266bfeea0c7502fecbb25bfb2fdf3b426f
William Nguyen <101022886+wqngyn@users.noreply.github.com>
2024-06-18 00:15:03 -0700

    fix isMounted useRef

--- src/components/Search/Search-Results.jsx
+++ src/components/Search/Search-Results.jsx
@@ -3,8 +3,8 @@ import SearchResultsRow from './SearchResultsRow'
 import { KeyConversionContext, compareCamelotKey } from '../../contexts/KeyConversionContext'
 import { UtilityContext } from '../../contexts/UtilityContext'
 import { TokenAuthContext } from '../../contexts/TokenAuthContext'
-
 import styles from './Search-Results.module.scss'
+
 
 const searchResults = ({ 
     searchResults, 
@@ -30,6 +30,7 @@ const searchResults = ({
     const { convertToKey, convertToCamelotKey } = useContext(KeyConversionContext);
     const { getRefreshToken } = useContext(TokenAuthContext);
     const { sortArr } = useContext(UtilityContext);
+    const isMounted = useRef(false);
 
     useEffect(() => {
         localStorage.setItem('trackIdArr', JSON.stringify(trackIdArr));
@@ -37,54 +38,44 @@ const searchResults = ({
         localStorage.setItem('renderedSearchResults', JSON.stringify(renderedSearchResults));
       }, [tracklistFeatures, renderedSearchResults, trackIdArr])
 
-    // This prevents fetch on first render (i.e. refresh) to prevent unneeded API calls. It becomes true after first fetch.
-    const didMount = useRef(false); 
-    const table = useRef();
-
     // Fetch audio features for whole tracklist.
     useEffect(() => {
 
         // 1. Push trackId from each result in searchResult to trackIdArr.
-        const arr = [];
+        const idArr = [];
         const searchResultsClone = structuredClone(searchResults);
         if(searchResultsClone) {
-            searchResultsClone.map(track => { arr.push(track.id); });
-            setTrackIdArr(arr);
+            searchResultsClone.map(track => { idArr.push(track.id); });
+            setTrackIdArr(idArr);
         }
         
         try {
             // 2. Using trackIdArr, fetch audio features for whole list.
             const fetchTracklistFeatures = async() => {
-                const trackIdArrStr = arr.join('%2C');
+                console.log('fetch')
+                const trackIdArrStr = idArr.join('%2C');
                 const response = await fetch(`https://api.spotify.com/v1/audio-features?ids=${trackIdArrStr}`, {
                     headers: { Authorization: 'Bearer ' + localStorage.getItem('access_token')}
                 });
                 const data = await response.json();
-                if(data.error && data.error.status === 401) {
-                    getRefreshToken();
+                (data.error && data.error.status === 401) ? getRefreshToken() : setTracklistFeatures(data.audio_features, setCurrentAsFirst(data.audio_features));
+            }
+
+            const setCurrentAsFirst = (audioFeatures) => {
+                setCurrentTrack(searchResults[0]);
+                setTrackFeatures(audioFeatures[0]);
+            }
+            
+            if(idArr.length !== 0) {
+                if(!isMounted.current) {
+                    const localTracklistFeatures = JSON.parse(localStorage.getItem('tracklistFeatures'));
+                    (!localTracklistFeatures) ? fetchTracklistFeatures() : setTracklistFeatures(localTracklistFeatures);
+                    isMounted.current = true;
                 } else {
-                    setTracklistFeatures(data.audio_features, setInitialValues(data.audio_features));
-                } 
-            }
-
-            const setInitialValues = (audioFeatures) => {
-                if(!didMount.current) {
-                    setCurrentTrack(searchResults[0]);
-                    setTrackFeatures(audioFeatures[0]);
-                    didMount.current = true;
-                }
-            }
-
-            if(arr.length !== 0 && !didMount.current) {
-                const localTracklistFeatures = JSON.parse(localStorage.getItem('tracklistFeatures'));
-                if(!localTracklistFeatures) {
                     fetchTracklistFeatures();
-                } else {
-                    setTracklistFeatures(localTracklistFeatures);
-                    didMount.current = true;
-                }
+                }   
             }
-
+          
         } catch(err) {
             console.error(err);
         }
@@ -92,10 +83,9 @@ const searchResults = ({
 
     // Add camelot, key, tempo, and key match properties to searchResults.
     useEffect(() => {
-        const searchResultsWithAudioFeatures = structuredClone(searchResults);
-      
+        const searchResultsWithFeatures = structuredClone(searchResults);
         if(tracklistFeatures) {
-            searchResultsWithAudioFeatures.forEach((result, index) => {
+            searchResultsWithFeatures.forEach((result, index) => {
                 result['removed'] = false;
                 result['key'] = convertToKey([tracklistFeatures[index].key, tracklistFeatures[index].mode]);
                 result['camelot'] = convertToCamelotKey([tracklistFeatures[index].key, tracklistFeatures[index].mode]);
@@ -104,13 +94,9 @@ const searchResults = ({
                     result['keyMatch'] = compareCamelotKey(convertToCamelotKey([trackFeatures.key, trackFeatures.mode]), result.camelot); 
             });
         }
-
-        setUnfilteredSearchResults(searchResultsWithAudioFeatures);
-        if(!filterActive) setRenderedSearchResults(searchResultsWithAudioFeatures);
-
+        setUnfilteredSearchResults(searchResultsWithFeatures);
+        if(!filterActive) setRenderedSearchResults(searchResultsWithFeatures);
     }, [trackFeatures, tracklistFeatures, searchResults])
-
-
 
      // Set unique key match filters.
      useEffect(() => {
@@ -133,7 +119,7 @@ const searchResults = ({
         let unfilteredSearchResultsCopy = structuredClone(unfilteredSearchResults);
 
         if(sort.active) sortArr(unfilteredSearchResultsCopy, sort.parameter, sort.ascending);
-
+        
         if(filterActive) {
             unfilteredSearchResultsCopy.forEach((result) => {
                 (selectedFilterType !== result.keyMatch) ? result.removed = true : result.removed = false;
@@ -147,7 +133,7 @@ const searchResults = ({
 
 
     return (
-        <div className={styles.searchResults} ref={table}>
+        <div className={styles.searchResults}>
             {/* Make tracklistFeatures required to prevent premature re-render (i.e., NaN BPM) */}
             {(renderedSearchResults && tracklistFeatures) && renderedSearchResults.map((track, index) => 
                 <SearchResultsRow key={track.id}
